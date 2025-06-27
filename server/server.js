1c00e5cc7476c9cb5ed27b7eaaf3cb70561ba388
LeelaMadhavaRao <leelamadhav.nulakani@gmail.com>
2025-06-27 18:06:12 +0530

    path for frontend corrected

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://poultry-expense-tracker-q5h2.vercel.app'
+    'https://poultry-expense-tracker-q5h2.vercel.app/'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
