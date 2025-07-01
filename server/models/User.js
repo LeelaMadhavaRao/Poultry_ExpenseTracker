37b06f8551e4c0bf0637eaabbeb9714c88788046
LeelaMadhavaRao <leelamadhav.nulakani@gmail.com>
2025-07-01 11:50:58 +0530

    remove unique constraint from email field in user schema

--- server/models/User.js
+++ server/models/User.js
@@ -7,7 +7,7 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
